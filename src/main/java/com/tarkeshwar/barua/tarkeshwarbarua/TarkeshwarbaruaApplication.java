ef42f362aed0c3043734d3383314296a9ca5cd69
Tarkeshwar <tbarua1@gmail.com>
2020-10-25 06:08:24 +0000

    First test with Jenkins

---
+++ src/main/java/com/tarkeshwar/barua/tarkeshwarbarua/TarkeshwarbaruaApplication.java
@@ -0,0 +1,13 @@
+package com.tarkeshwar.barua.tarkeshwarbarua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarkeshwarbaruaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarkeshwarbaruaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarkeshwar/barua/tarkeshwarbarua/TarkeshwarbaruaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarkeshwar.barua.tarkeshwarbarua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarkeshwarbaruaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
